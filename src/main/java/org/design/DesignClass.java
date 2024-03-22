a6d4f52b740c67f71375b59901f9857191da5a09
Adelin Bojan <adelinbojan96@gmail.com>
2024-03-22 09:42:10 +0200

    finish design class

--- src/main/java/org/design/DesignClass.java
+++ src/main/java/org/design/DesignClass.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class DesignClass {
     public static void customizeButton(JButton button)
